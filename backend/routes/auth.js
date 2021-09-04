6d61636d49d28e568dc11e673d28bdb2ea6d24c6
Sujeet Tiwary <sujeetiwary1@gmail.com>
2021-09-05 03:18:31 +0530

    Securing Password and JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = "FellowDev"
 // Create a User using : POST "/api/auth/createuser", No Login is Required
 router.post(
   "/createuser",
@@ -28,12 +31,21 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exists" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt)
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const authToken = jwt.sign(data, JWT_SECRET);
+      
+      res.json({ authToken });
     } catch (error) {
       res.status(500).send({ error: "Some error occured" });
       console.error(error);
